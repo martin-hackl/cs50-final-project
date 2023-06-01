850ee93d71971fba243f59a534136a1477ce59cd
Martin Hackl <martin.hackl@bnet.at>
2023-06-01 21:06:55 +0200

    create missing html sites and implement active status in navbar

--- app.py
+++ app.py
@@ -5,8 +5,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", activeHome="active")
 
 @app.route("/metronome")
 def metronome():
-    return render_template("metronome.html")
+    return render_template("metronome.html", activeMetronome="active")
+
+@app.route("/tap-tempo")
+def tapTempo():
+    return render_template("tap-tempo.html", activeTaptempo="active")
+
+@app.route("/tuner")
+def tuner():
+    return render_template("tuner.html", activeTuner="active")
